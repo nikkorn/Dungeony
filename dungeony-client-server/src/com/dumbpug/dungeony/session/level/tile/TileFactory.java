c9057dc6d22ff2fc0d8bda5694a085558362a1ce
Nikolas Howard <nikolas.howard@LighthouseSystems.com>
2019-06-11 13:28:35 +0100

    Working on TileFactory

--- dungeony-client-server/src/com/dumbpug/dungeony/session/level/tile/TileFactory.java
+++ dungeony-client-server/src/com/dumbpug/dungeony/session/level/tile/TileFactory.java
@@ -1,6 +1,6 @@
 package com.dumbpug.dungeony.session.level.tile;
 
-import com.dumbpug.dungeony.session.level.Direction;
+import dungen.tile.ITileDetails;
 
 /**
  * Factory for creating Tile instances.
@@ -8,12 +8,47 @@ import com.dumbpug.dungeony.session.level.Direction;
 public class TileFactory {
 	
 	/**
-	 * Create a Tile instance.
-	 * @param type The tile type
-	 * @param direction The tile direction.
-	 * @return The created Tile instance.
+	 * Create a Tile instance based on details of a generated tile.
+	 * @param generated The generated tile details.
+	 * @returns A Tile instance based on details of a generated tile.
 	 */
-	public static Tile createTile(TileType type, Direction direction) {
+	public static Tile createTile(ITileDetails generated) {
+		switch (generated.getType()) {
+			case EMPTY:
+				return createEmptyTile(generated);
+			case ENTRANCE:
+				return createDoorTile(generated);
+			case WALL:
+				return createWallTile(generated);
+			default:
+				throw new RuntimeException("unknown generated time type: " + generated.getType());
+		}
+	}
+	
+	/**
+	 * Create an empty tile based on details of a generated tile.
+	 * @param generated The generated tile details.
+	 * @returns An empty tile based on details of a generated tile.
+	 */
+	private static Tile createEmptyTile(ITileDetails generated) {
+		return null;
+	}
+	
+	/**
+	 * Create a door tile based on details of a generated tile.
+	 * @param generated The generated tile details.
+	 * @returns A door tile based on details of a generated tile.
+	 */
+	private static Tile createDoorTile(ITileDetails generated) {
+		return null;
+	}
+	
+	/**
+	 * Create a wall tile based on details of a generated tile.
+	 * @param generated The generated tile details.
+	 * @returns A wall tile based on details of a generated tile.
+	 */
+	private static Tile createWallTile(ITileDetails generated) {
 		return null;
 	}
 }
